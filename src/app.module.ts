1c582f7a3070ad47b18b7958579e4cde79384b6b
willian <williandeitosi@gmail.com>
2024-05-29 13:54:37 -0300

    feat: prisma database and prisma service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PostModule } from './post/post.module';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [PostModule],
   controllers: [],
-  providers: [],
+  providers: [PrismaService],
 })
 export class AppModule {}
