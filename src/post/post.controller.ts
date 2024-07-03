2b80c32d5468d7a28bbafaf86c2d9643f84a653c
willian <williandeitosi@gmail.com>
2024-07-03 19:25:19 -0300

    fix: insirt ispublic in find all and find one

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -7,6 +7,7 @@ import {
   Post,
   Put,
 } from '@nestjs/common';
+import { IsPublic } from 'src/auth/decorators/Is-public.decorator';
 import { CreatePostDto } from './dto/create-post.dto';
 import { PostService } from './post.service';
 
@@ -14,10 +15,12 @@ import { PostService } from './post.service';
 export class PostController {
   constructor(private readonly postService: PostService) {}
 
+  @IsPublic()
   @Get()
   async findAll() {
     return await this.postService.findAll();
   }
+  @IsPublic()
   @Get(':id')
   async findOne(@Param('id') id: string) {
     return await this.postService.findOne(id);
